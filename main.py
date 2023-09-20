121292a9ad7803ef1f7a561350829483e5cfe403
Shavileyolo <shavileyolo@gmail.com>
2023-09-20 16:33:14 +0300

    added save in xlsx file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from openpyxl import Workbook
 
 # Отправляем GET-запрос
 URL_TEMPLATE = 'https://www.noob-club.ru/'
@@ -13,9 +14,24 @@ soup = bs(r.text, "html.parser")
 
 #Заголовок
 title = soup.title.text
-print(title)
+#print(title)
 
 #Текст из тега
 tag_text = soup.find_all('span', class_='entry-header')
+tags = []
 for text in tag_text:
-    print(text.text)
+    tags.append(text.text)
+    #print(text.text)
+
+
+#Сохраняем результаты в файле
+#Создание таблицы
+wb = Workbook()
+sheet = wb.active
+
+#Запись в таблицу
+for i, tag in enumerate(tags):
+    sheet.cell(row = i+1, column = 1, value = tag)
+
+#Сохраняем результаты
+wb.save('test.xlsx')
